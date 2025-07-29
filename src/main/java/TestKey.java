e273c61daeb5466d1839b7d3e2dd9c7653b3e6a8
dxa204 <82316603+dxa204@users.noreply.github.com>
2025-07-29 10:49:37 -0400

    Delete old files

--- src/main/java/TestKey.java
+++
@@ -1,7 +0,0 @@
-public class TestKey {
-    public static void main(String[] args) {
-        // Direct reference WITHOUT import:
-        javafx.scene.input.KeyCode code = javafx.scene.input.KeyCode.A;
-        System.out.println(code);
-    }
-}
