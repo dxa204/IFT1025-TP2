4e99143bd2fa020d69186e8f9a3e4cca4b2de13c
CharlesLaf51 <charl.lafontaine@gmail.com>
2025-07-31 01:29:45 -0400

    Update FlappyGhost.java

--- src/FlappyGhost.java
+++ src/FlappyGhost.java
@@ -1,5 +1,10 @@
-// Derin Akay 20234040
-// Charles Lafontaine 20279271
+/**
+ * Classe principale qui initialise l'application JavaFX et lance le contrôleur du jeu.
+ *
+ * @author Derin Akay 20234040
+ * @author Charles Lafontaine 20279271
+ */
+
 
 import javafx.application.Application;
 import javafx.stage.Stage;
@@ -8,16 +13,28 @@ import javafx.scene.image.Image;
 
 
 public class FlappyGhost extends Application {
+    /**
+     * Appelée lors du démarrage de l'application.
+     * Initialise la fenêtre principale (Stage) et crée le contrôleur du jeu.
+     *
+     * @param stage La fenêtre principale de l'application.
+     */
     @Override
     public void start(Stage stage) {
+        // Crée et initialise le contrôleur du jeu avec la fenêtre principale
         new GameController(stage);
 
-        // add the ghost icon into the title‐bar
+        // Ajoute l'icône du fantôme dans la barre de titre
         stage.getIcons().add(
                 new Image(getClass().getClassLoader().getResourceAsStream("ghost.png"))
         );
     }
 
+    /**
+     * Lance l'application JavaFX.
+     *
+     * @param args Les arguments de la ligne de commande (pas utiles ici).
+     */
     public static void main(String[] args) {
         launch(args);
     }
